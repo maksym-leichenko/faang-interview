4b9e8254cccc552881bdd1866b161c570cb4d43d
Maksym Leichenko <ky61k105@gmail.com>
2021-05-29 18:56:23 +0300

    feat: added next auth

--- src/middleware/mongodb.ts
+++ src/middleware/mongodb.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 const databaseMiddleware = async (req: NextApiRequest, res: NextApiResponse, next: Function) => {
   try {
     if (!global.mongoose) {
-      global.mongoose = await mongoose.connect(MONGODB_URL, {
+      global.mongoose = await mongoose.connect(DATABASE_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -1,11 +1,23 @@
 // Import FirebaseAuth and firebase.
 import React from 'react';
+import { useSession, signIn, signOut } from 'next-auth/client';
 
 function SignInScreen() {
+  const [session, loading] = useSession();
+  const handleSignin = (event) => {
+    event.preventDefault()
+    signIn()
+  }
+
+  const handleSignout = (event) => {
+    event.preventDefault()
+    signOut()
+  }
   return (
     <div>
       <h1>My App</h1>
-      <p>Please sign-in:</p>
+       {session && <a href="#" onClick={handleSignout} className="btn-signin">Sign out</a>  }
+       {!session && <a href="#" onClick={handleSignin}  className="btn-signin">Sign in</a>  }
     </div>
   );
 }
