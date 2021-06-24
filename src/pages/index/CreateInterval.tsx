440786207395f5f5fc54bdcc133299eccd402c08
Maksym Leichenko <ky61k105@gmail.com>
2021-06-25 01:41:58 +0300

    feat: added final forms

--- src/pages/index/CreateInterval.tsx
+++ src/pages/index/CreateInterval.tsx
@@ -1,15 +1,8 @@
 import 'date-fns';
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import TextField from '@material-ui/core/TextField';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
@@ -22,11 +15,8 @@ import DateFnsUtils from '@date-io/date-fns';
 import Dialog from '@material-ui/core/Dialog';
 import Slide from '@material-ui/core/Slide';
 import { TransitionProps } from '@material-ui/core/transitions';
-import {
-  MuiPickersUtilsProvider,
-  KeyboardTimePicker,
-  KeyboardDatePicker,
-} from '@material-ui/pickers';
+import { Form } from 'react-final-form';
+import { TextField, Checkboxes, Radios, DatePicker, TimePicker } from 'mui-rff';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -52,174 +42,160 @@ type ICreateIntervalProps = {
   handleClose: () => void,
 }
 
+interface IIntervalData {
+  mocksCount: number;
+  dateFrom: string;
+  dateTo: string;
+  isPublic: string;
+  mockTypes: string[];
+}
 
 export default function CreateInterval({ open, handleClose }: ICreateIntervalProps) {
   const classes = useStyles();
-  const [fromDate, setFromDate] = React.useState<Date | null>(new Date());
-  const [toDate, setToDate] = React.useState<Date | null>(new Date());
-  const [isPublic, setIsPublic] = React.useState(true);
-  const [mockTypes, setMockTypes] = React.useState({
-    algo: true,
-    sd: false,
-    'sd-front': false,
-    'sd-mobile': false,
-    front: false,
-  });
 
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setMockTypes({ ...mockTypes, [event.target.name]: event.target.checked });
+  const onSubmit = async (values: IIntervalData) => {
+    // @ts-ignore
+    window.alert(JSON.stringify(values, 0, 2));
   };
 
-  const handlePublicChange = () => {
-    setIsPublic(!isPublic);
-  };
-
-
-  const handleDateChange = (date: Date | null) => {
-    setFromDate(date);
-    setToDate(date);
-  };
-
-  const submitHandler = async () => {
-
-  };
-
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(event);
-    submitHandler();
+  const validate = (values: IIntervalData) => {
+    const errors: { [key: string]: string } = {};
+    if (!values.mocksCount) {
+      errors.mocksCount = 'Required';
+    }
+    if (values?.mocksCount < 1) {
+      errors.mocksCount = 'Required';
+    }
+    if (!values.dateFrom) {
+      errors.dateFrom = 'Required';
+    }
+    if (!values.dateTo) {
+      errors.dateTo = 'Required';
+    }
+    return errors;
   };
 
   return (
     <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
-      <AppBar className={classes.appBar}>
-        <Toolbar>
-          <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
-            <CloseIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Create new interval
-          </Typography>
-          <Button autoFocus color="inherit" onClick={() => submitHandler()}>
-            save
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <Container maxWidth={false}>
-        <form onSubmit={onSubmit}>
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container spacing={3}>
-              <Grid item xs={12} lg={3}>
-                <KeyboardDatePicker
-                  margin="normal"
-                  id="date-picker-dialog"
-                  label="Date when u avalible for mock"
-                  format="MM/dd/yyyy"
-                  value={fromDate}
-                  onChange={handleDateChange}
-                  KeyboardButtonProps={{
-                    'aria-label': 'change date',
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6} lg={3}>
-                <KeyboardTimePicker
-                  margin="normal"
-                  id="time-picker-from"
-                  label="Time from u avalible for mock"
-                  value={fromDate}
-                  onChange={setFromDate}
-                  KeyboardButtonProps={{
-                    'aria-label': 'change time',
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6} lg={3}>
-                <KeyboardTimePicker
-                  margin="normal"
-                  id="time-picker-to"
-                  label="Time to u avalible for mock"
-                  value={toDate}
-                  onChange={setToDate}
-                  KeyboardButtonProps={{
-                    'aria-label': 'change time',
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl component="fieldset" fullWidth>
-                  <TextField
-                    id="standard-full-width"
-                    label="How many mock interview do u want for this period?"
-                    style={{ margin: 8 }}
-                    placeholder=""
-                    helperText=""
-                    fullWidth
+      <Form
+        onSubmit={onSubmit}
+        initialValues={{ isPublic: "yes", mocksCount: 1, mockTypes: ['algo'] }}
+        validate={validate}
+        render={({ handleSubmit, form, submitting, pristine, values }) => (
+          <form onSubmit={handleSubmit} noValidate>
+            <AppBar className={classes.appBar}>
+              <Toolbar>
+                <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+                  <CloseIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                  Create new interval
+                </Typography>
+                <Button
+                  autoFocus
+                  color="inherit"
+                  type="submit"
+                  disabled={submitting}>
+                  save
+                </Button>
+              </Toolbar>
+            </AppBar>
+            <Container maxWidth={false}>
+              <Grid container spacing={3}>
+                <Grid item xs={12} lg={3}>
+                  <DatePicker
+                    label="Date when u avalible for mock"
+                    format="MM/dd/yyyy"
+                    name="dateFrom"
                     margin="normal"
-                    InputLabelProps={{
-                      shrink: true,
-                    }}
+                    dateFunsUtils={DateFnsUtils}
                   />
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl component="fieldset" fullWidth>
-                  <FormLabel component="legend">What type of mock u want practice?</FormLabel>
-                  <FormGroup>
-                    <FormControlLabel
-                      control={<Checkbox checked={mockTypes.algo} onChange={handleChange} name="algo" />}
-                      label="Algo mocks"
+                </Grid>
+                <Grid item xs={12} sm={6} lg={3}>
+                  <TimePicker
+                    name="dateFrom"
+                    margin="normal"
+                    label="Time from u avalible for mock (FROM)"
+                    dateFunsUtils={DateFnsUtils}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6} lg={3}>
+                  <TimePicker
+                    name="dateTo"
+                    margin="normal"
+                    label="Time from u avalible for mock (TO)"
+                    dateFunsUtils={DateFnsUtils}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl component="fieldset" fullWidth>
+                    <TextField
+                      id="standard-full-width"
+                      label="How many mock interview do u want for this period?"
+                      style={{ margin: 8 }}
+                      placeholder=""
+                      helperText=""
+                      fullWidth
+                      type="number"
+                      margin="normal"
+                      name="mocksCount"
+                      required={true}
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
                     />
-                    <FormControlLabel
-                      control={<Checkbox checked={mockTypes.sd} onChange={handleChange} name="sd" />}
-                      label="Syctem design mocks"
-                    />
-                    <FormControlLabel
-                      control={<Checkbox checked={mockTypes['sd-front']} onChange={handleChange} name="sd-front" />}
-                      label="Syctem design mocks (Front-end)"
-                    />
-                    <FormControlLabel
-                      control={<Checkbox checked={mockTypes['sd-mobile']} onChange={handleChange} name="sd-mobile" />}
-                      label="Syctem design mocks (Mobile)"
-                    />
-                    <FormControlLabel
-                      control={<Checkbox checked={mockTypes.front} onChange={handleChange} name="front" />}
-                      label="Front-end tasks"
-                    />
-                  </FormGroup>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <Checkboxes
+                    label="What type of mock u want practice?"
+                    name="mockTypes"
+                    formControlProps={{ margin: 'none' }}
+                    data={[
+                      { label: 'Algo mocks', value: 'algo' },
+                      { label: 'Syctem design mocks', value: 'sd' },
+                      { label: 'Syctem design mocks (Front-end)', value: 'sdFront' },
+                      { label: 'Syctem design mocks (Mobile)', value: 'sdMobile' },
+                      { label: 'Front-end tasks', value: 'front' },
+                    ]}
+                  />
                   <FormHelperText>Select minimum one type</FormHelperText>
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl component="fieldset" fullWidth>
-                  <FormLabel component="legend">Dont u midnt this mock will be public?</FormLabel>
-                  <RadioGroup aria-label="quiz" name="quiz" value={isPublic} onChange={handlePublicChange}>
-                    <FormControlLabel value={true} control={<Radio />} label="Yea, okey!" />
-                    <FormControlLabel value={false} control={<Radio />} label="No, next time." />
-                  </RadioGroup>
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl component="fieldset" fullWidth>
-                  <TextField
-                    id="standard-full-width-comment"
-                    label="Do u want add something?"
-                    style={{ margin: 8 }}
-                    placeholder="Type ur comment here.."
-                    helperText=""
-                    fullWidth
-                    margin="normal"
-                    InputLabelProps={{
-                      shrink: true,
-                    }}
+
+                </Grid>
+                <Grid item xs={12}>
+                  <Radios
+                    label="Dont u midnt this mock will be public?"
+                    name="isPublic"
+                    formControlProps={{ margin: 'none' }}
+                    data={[
+                      { label: 'Yea, okey!', value: "yes" },
+                      { label: 'No, next time.', value: "no" },
+                    ]}
                   />
-                </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControl component="fieldset" fullWidth>
+                    <TextField
+                      id="standard-full-width-comment"
+                      label="Do u want add something?"
+                      style={{ margin: 8 }}
+                      placeholder="Type ur comment here.."
+                      helperText=""
+                      fullWidth
+                      margin="normal"
+                      name="notes"
+                      multiline
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                    />
+                  </FormControl>
+                </Grid>
               </Grid>
-            </Grid>
-          </MuiPickersUtilsProvider>
-        </form>
-      </Container>
+            </Container>
+          </form>
+        )}
+      />
     </Dialog>
   );
 }
